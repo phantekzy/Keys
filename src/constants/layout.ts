d759b42d328aada3a3457eb14c6635656f3ff428
phantekzy <mainilotfi@gmail.com>
2026-04-09 08:45:35 +0100

    a keyboard is a grid so we have to make different rows -Row number 1

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,3 +1,19 @@
 import type { KeyConfig } from "../types/keyboard";
 
-export const KEYBOARD_LAYOUT : KeyConfig[][]=[]
+export const KEYBOARD_LAYOUT: KeyConfig[][] = [
+  [
+    { code: "Escape", label: "ESC" },
+    { code: "F1", label: "F1" },
+    { code: "F2", label: "F2" },
+    { code: "F3", label: "F3" },
+    { code: "F4", label: "F4" },
+    { code: "F5", label: "F5" },
+    { code: "F6", label: "F6" },
+    { code: "F7", label: "F7" },
+    { code: "F8", label: "F8" },
+    { code: "F9", label: "F9" },
+    { code: "F10", label: "F10" },
+    { code: "F11", label: "F11" },
+    { code: "F12", label: "F12" },
+  ],
+];
