67d72d5a855748f891e453cd6a35a11c6b05fa30
phantekzy <mainilotfi@gmail.com>
2026-04-10 20:11:36 +0100

    flexible styling

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -3,5 +3,15 @@ import type { KeyConfig, KeyStatus } from "../types/keyboard";
 interface Props {
     config: KeyConfig;
     status: KeyStatus;
+}
 
+export const Key = ({ config, status }: Props) => {
+    return (
+        <div
+            className={`key-cap ${status}`}
+            style={{ flexGrow: config.width || 1, flexBasis: 0 }}
+        >
+
+        </div>
+    )
 }
