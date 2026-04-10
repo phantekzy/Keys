9ef799339236ac70dc8a97ac0f84962bc9373098
phantekzy <mainilotfi@gmail.com>
2026-04-10 20:20:49 +0100

    got the keyboardState from the hook and now i have to make the css so i can continue

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,3 +1,12 @@
+import { useKeyboard } from "../hooks/useKeyboard"
 
 
-export const Keyboard = () => { }
+export const Keyboard = () => {
+    const { keyState } = useKeyboard();
+
+    return (
+        <div className="keyboard-frame">
+
+        </div>
+    )
+}
